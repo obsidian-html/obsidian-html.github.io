8d5fe0ea38c5ee20982553725a4e361b22875c0f
dwrolvink <dwrolvink@gmail.com>
2022-09-16 21:18:16 +0200

    autopush

--- index_from_tags/obs.html/static/obsidian_core.js
+++ index_from_tags/obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/index_from_tags";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '240669707630116222941019656907668217424'                       // used to check whether the localStorage data is stale
+var gzip_hash = '89279544441170377811396930130509634144'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '297117576757286849795355180397816738375'                       // used to check whether the localStorage data is stale
+var gzip_hash = '278936923079700510483170411954705471439'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
