5ababdea9a47217489a2ff7b49a0875e590ef381
dwrolvink <dwrolvink@gmail.com>
2023-03-12 20:06:55 +0100

    autopush

--- index_from_tags/obs.html/static/obsidian_core.js
+++ index_from_tags/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/index_from_tags";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '302222537902881017630477765529502490765'                       // used to check whether the localStorage data is stale
+var gzip_hash = '261182826314686860051155407920419543464'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
